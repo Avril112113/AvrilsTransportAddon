bd52801483a2333ca22aabe1be62994a629e2d00
Dude112113 <curtisdude12@gmail.com>
2023-07-24 18:23:54 +0100

    Fixed types for fluids.

--- config/producibles/fluids.lua
+++ config/producibles/fluids.lua
@@ -3,22 +3,22 @@
 
 LocationSystem
 	.addProducible(
-		ProducibleConfig.new("diesel", PRODUCIBLE_TYPES.mineral)
+		ProducibleConfig.new("diesel", PRODUCIBLE_TYPES.fluid)
 			:setFluidType(1)
 	)
 	.addProducible(
-		ProducibleConfig.new("jetfuel", PRODUCIBLE_TYPES.mineral)
+		ProducibleConfig.new("jetfuel", PRODUCIBLE_TYPES.fluid)
 			:setFluidType(2)
 	)
 	.addProducible(
-		ProducibleConfig.new("oil", PRODUCIBLE_TYPES.mineral)
+		ProducibleConfig.new("oil", PRODUCIBLE_TYPES.fluid)
 			:setFluidType(5)
 	)
 	.addProducible(
-		ProducibleConfig.new("slurry", PRODUCIBLE_TYPES.mineral)
+		ProducibleConfig.new("slurry", PRODUCIBLE_TYPES.fluid)
 			:setFluidType(8)
 	)
 	-- .addProducible(
-	-- 	ProducibleConfig.new("slurry saturated", PRODUCIBLE_TYPES.mineral)
+	-- 	ProducibleConfig.new("slurry saturated", PRODUCIBLE_TYPES.fluid)
 	-- 		:setFluidType(9)
 	-- )
